0656aeef38625022deaec0d80f9e0954c1d6934c
Riju-vv <riju.vv@progress.com>
2021-11-24 06:57:55 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# web_docker_host
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: web_docker_host
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
