4ed469eb90d9859ecab39558d747eeb8f258dc80
Riju-vv <riju.vv@progress.com>
2021-11-24 10:07:48 +0000

    adding template and creating dir in home

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,4 +5,14 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 apt_update
 package 'net-tools'
+
+directory '/home/ubuntu/webcontainer' do
+  action :create
+end
+
+template '/home/ubuntu/webcontainer/index.html' do
+  source 'index.html.erb'
+  action :create
+end
+
 include_recipe 'web_docker_host::docker_setup'
