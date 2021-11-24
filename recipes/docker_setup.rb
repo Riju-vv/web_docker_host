45007eadcd61e8e6aa9b466587049ff3ac764a59
Riju-vv <riju.vv@progress.com>
2021-11-24 10:17:43 +0000

    using supermarket for haproxy and httpd

--- recipes/docker_setup.rb
+++ recipes/docker_setup.rb
@@ -14,3 +14,25 @@ group 'docker' do
   append true
   action :modify
 end
+
+docker_image 'haproxy' do
+  action :pull
+end
+
+docker_image 'httpd' do
+  action :pull
+end
+
+begin_count = 0
+end_count = node['web_docker_host']['webcontainer_count']
+while begin_count < end_count
+  docker_container "httpd-#{begin_count}" do
+    repo 'httpd'
+    volumes ['/home/ubuntu/webcontainer/:/usr/local/apache2/htdocs/']
+    port "800#{begin_count}:80"
+    read_timeout 60
+    write_timeout 60
+    action :run
+  end
+  begin_count += 1
+end
